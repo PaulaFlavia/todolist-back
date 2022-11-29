87b0a25ea65e318562df6d8487f11468049d5a3d
PaulaFlavia <paulaflavia@gmail.com>
2022-11-29 11:20:31 -0300

    tratando response

--- public/entrar.js
+++ public/entrar.js
@@ -1,3 +1,4 @@
+
 // Capturar o elementos
 let inputNome = document.getElementById("regNome");
 let inputEmail = document.getElementById("regEmail");
@@ -5,26 +6,63 @@ let inputSenha = document.getElementById("regSenha");
 let inputConf = document.getElementById("regConf");
 let formRegistrar = document.getElementById("form-registrar");
 
-// 
-const urlBase = 'https://todolist-0622.herokuapp.com';
+//
+const urlBase = "/api";
+const tratarResposta = async(response) => {
+  switch (response.status) {
+    case 201:
+    // capturar o token respondido pelo servidor 
+        let conteudo = await response.json();
+    //guardar o token na sessionStorage
+        sessionStorage.setItem('token', conteudo.token);
+        sessionStorage.setItem('usuario', JSON.stringify(conteudo.usuario));
+    //Carregar a index.html
+        location = 'index.html'
+    break;
 
+    case 409:
+        alert("Usuário já cadastrado")
+      break;
+
+    case 422:
+        alert("Senha  preenchida incorretamente")
+      break;
+
+    default:
+        alert("Erro inesperado")
+      break;
+  }
+};
 
 const registrarUsuario = async (dados) => {
-    // enviar os dados para o endereço de registro
-}
+  //1- Definir a Url completa para onde será enviada
+  let url = `${urlBase}/auth/registrar`;
+  //2-Preparar opções de envio
+  let opcoes = {
+    method: "POST",
+    body: JSON.stringify(dados),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  //3-Enviar os dados da Req para o end de registro
+  let response = await fetch(url, opcoes);
+  //4- tratar a resposta, usar a propriedade status do response
+  tratarResposta(response);
+};
 
 // Listeners
 const onFormRegistrarSubmit = (evt) => {
-    evt.preventDefault();
-    let dados = {
-        nome: inputNome.value,
-        email: inputEmail.value,
-        senha: inputSenha.value,
-        confirmacao: inputConf.value,
-    }
+  evt.preventDefault();
+  let dados = {
+    nome: inputNome.value,
+    email: inputEmail.value,
+    senha: inputSenha.value,
+    confirmacao: inputConf.value,
+  };
 
-    registrarUsuario(dados)
-}
+  registrarUsuario(dados);
+};
 
 // Associando listeners
-formRegistrar.addEventListener('submit', onFormRegistrarSubmit)
+formRegistrar.addEventListener("submit", onFormRegistrarSubmit);
